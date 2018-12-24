4b5dde45efa5adc3bc91352a5fad96b8e0679f63
HaoDaWang <1306036576@qq.com>
2018-12-24 20:15:54 +0800

    Wrap input component

---
+++ src/renderer/components/input.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="input">
+    <input @change="change($event)" :class="sizeType" type="text">
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    /** fill or auto */
+    sizeType: {
+      type: String,
+      default: "auto"
+    },
+    change: {
+      type: Function,
+      default: () => {}
+    }
+  }
+};
+</script>
+
+<style>
+.fill {
+  width: 100% !important;
+}
+
+.auto {
+  width: fit-content !important;
+}
+
+.input input {
+  height: 30px;
+  border: none;
+  outline: none;
+  border-radius: 1000px;
+  padding: 0 10px;
+  font-size: 16px;
+}
+</style>
