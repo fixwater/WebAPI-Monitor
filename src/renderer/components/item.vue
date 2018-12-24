1d36c589e0b9120e4eded9b45365033574781110
HaoDaWang <1306036576@qq.com>
2018-12-24 20:16:41 +0800

    Wrap item component

---
+++ src/renderer/components/item.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="item">
+    <span class="label-text">{{labelText}}:</span>
+    <Input :change="handleInputChange" class="info-input" type="text" sizeType="fill"/>
+  </div>
+</template>
+
+<script>
+import Input from "./input.vue";
+import { mapActions } from "vuex";
+import { data } from "../config/form";
+
+export default {
+  props: ["labelText", "inputSizeType"],
+  components: {
+    Input
+  },
+  methods: {
+    handleInputChange(e) {
+      data[this.$props.labelText] = e.target.value;
+    }
+  }
+};
+</script>
+
+<style>
+.item {
+  width: 100%;
+  padding: 5px 0;
+}
+
+.label-text {
+}
+
+.info-input {
+}
+</style>
