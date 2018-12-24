59d0a6570dae869e7dddadcd95b3e70f015d2abb
HaoDaWang <1306036576@qq.com>
2018-12-24 20:16:58 +0800

    Concat component

---
+++ src/renderer/components/main.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <Control></Control>
+    <Terminal></Terminal>
+  </div>
+</template>
+
+<script>
+import Control from "./control.vue";
+import Terminal from "./terminal.vue";
+
+export default {
+  components: {
+    Control,
+    Terminal
+  }
+};
+</script>
+
+<style>
+.container {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+</style>
