bb0b69901cfa38b04388d9405716d0f6704d4dd6
HaoDaWang <1306036576@qq.com>
2018-12-24 20:17:09 +0800

    For output

---
+++ src/renderer/components/terminal.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="terminal">
+    <p class="info" v-for="info in infos" :key="info.key">
+      <strong>$ {{info}}</strong>
+    </p>
+  </div>
+</template>
+
+<script>
+import { data } from "../config/form";
+
+export default {
+  data: () => {
+    return {
+      infos: data.infos
+    };
+  }
+};
+</script>
+
+<style>
+.terminal {
+  width: 100%;
+  height: 100%;
+  background: #000;
+  padding: 5px;
+  overflow: scroll;
+}
+
+.info {
+  color: green;
+}
+</style>
